44d3e80ca34abe0cca3d5c42cca05738c87347d3
Hari Chalise <ingoprivate@gmail.com>
2025-01-13 23:47:59 +0545

    plugin added

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -140,10 +140,21 @@ return {
     "wurli/visimatch.nvim",
     lazy = false,
     opts = {}
-}
+  },
 
+  {
+    "z0mbix/vim-shfmt",
+    lazy=false,
+  },
 
-
+  {
+    "rachartier/tiny-glimmer.nvim",
+    lazy = false,
+    event = "TextYankPost",
+    opts = {
+        -- your configuration
+    },
+  }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
